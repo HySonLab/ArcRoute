c93288e551fa0246efba0aca43100e00241a4441
quangvan99 <vanquang.tpa@gmail.com>
2024-10-12 16:27:40 +0000

    fix benchmark

--- baseline/benchmark.py
+++ baseline/benchmark.py
@@ -1,82 +1,32 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from baseline.hr import InsertCheapestHCARP
-from common.local_search import local_search
-from common.cal_reward import get_Ts
-from rl.ppo import PPO
-from common.ops import import_instance, batchify
-
-import glob
-from tqdm import tqdm
+from baseline.meta_models import InsertCheapestHCARP, EAHCARP, ACOHCARP
+from time import time
+from glob import glob
 import numpy as np
-import torch
-
 
 if __name__ == "__main__":
-    torch.manual_seed(6868)
     np.random.seed(6868)
-    files = glob.glob('/usr/local/sra/testing_data/medium/*.npz')
-    files = sorted(files, key=lambda x : int(x.split('/')[-1].split('_')[0]))[:100]
-    results = {
-        "hr": [],
-        "rl": []
-    }
-    
-    # Heuristics
-    # al = InsertCheapestHCARP()
-    # for f in tqdm(files):
-    #     al.import_instance(f)
-    #     routes = al(merge_tour=True)
-    #     vars = {
-    #         'adj': al.dms[None],
-    #         'service_time': al.s[None],
-    #         'clss': al.clss[None],
-    #         'demand': al.demands[None]
-    #     }
-    #     tours = local_search(vars, actions=routes[None], variant='P')
-    #     r = get_Ts(vars, tours_batch=tours)
-    #     results['hr'].append(r[0])
-    # results['hr'] = np.array(results['hr'])
+    files = glob('/usr/local/sra/ArcRoute/data/instances/*/*.npz')[:10]
 
-    # print(results['hr'][:, 0])
-    # exit()
+    print('------------------')
+    al = EAHCARP(n_population=200) # EA
+    for f in files:
+        al.import_instance(f)
+        t1 = time()
+        print(f,':::' , al(n_epoch=1, variant='P'),':::' , time() - t1)
 
-        
-    # Hybrid Reinforce
-    model = PPO.load_from_checkpoint('/usr/local/sra/cpkts/bestP_20_2.ckpt')
-    policy, env = model.policy.cuda(), model.env
-    for f in tqdm(files):
-        dms, P, M, demands, clss, s, d, edge_indxs = import_instance(f)
-        td = env.reset(batch_size=1)
-        td['clss'] = torch.tensor(clss[None, :], dtype=torch.int64)
-        td['demand'] = torch.tensor(demands[1:][None, :], dtype=torch.float32)
-        td['service_time'] = torch.tensor(s[None, :], dtype=torch.float32)
-        td['traveling_time'] = torch.tensor(d[None, :], dtype=torch.float32)
-        td['adj'] = torch.tensor(dms[None, :], dtype=torch.float32)
-        td = td.cuda()
+    print('------------------')
+    al = ACOHCARP(n_ant=100) # ACO
+    for f in files:
+        al.import_instance(f)
+        t1 = time()
+        print(f,':::', al(n_epoch=1, variant='P'),':::', time() - t1)
 
-        # td = batchify(td, 100)
-        # with torch.no_grad():
-        #     out = policy(td, env=env, decode_type='sampling', return_actions=True)
-        #     obj = env.get_objective(td, out['actions'])
-        #     idx = obj[:, 0].argmin()
-        #     results['rl'].append(obj[idx].numpy())
-
-        with torch.no_grad():
-            out = policy(td, env=env, decode_type='greedy', return_actions=True)
-            obj = env.get_objective(td, out['actions'])
-            if obj[0].sum() >= 1000:
-                print(out['actions'])
-                exit()
-            results['rl'].append(obj[0])
-    
-    results['rl'] = np.array(results['rl'])
-
-    print(results['rl'])
-    exit()
-    
-    r1_rl = results['rl'][:, 0]
-    r1_hr = results['hr'][:, 0]
-    
-    print("Gap%: hr - rl:", ((r1_hr - r1_rl)/(r1_rl+1e-8) * 100).mean())
+    print('------------------')
+    al = InsertCheapestHCARP() # ACO
+    for f in files:
+        al.import_instance(f)
+        t1 = time()
+        print(f,':::', al(variant='P'),':::', time() - t1)
--- baseline/meta_models.py
+++ baseline/meta_models.py
@@ -1,15 +1,14 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import numpy as np
-from numpy.random import seed, random, randint, permutation
-
-from common.ops import import_instance, softmax
-from common.nb_utils import gen_tours, gen_tours_batch, deserialize_tours, deserialize_tours_batch
+from common.ops import import_instance, convert_prob
+from common.nb_utils import gen_tours, deserialize_tours, deserialize_tours_batch
 from common.cal_reward import get_Ts
-from common.local_search import local_search
+import numpy as np
+from numpy.random import random, randint, permutation
+from common.local_search import ls
 
-class InsertCheapestHCARP:
+class BaseHCARP:
     def __init__(self):
         self.has_instance = False
 
@@ -19,11 +18,24 @@ class InsertCheapestHCARP:
         self.demands, self.clss, self.s, self.d = demands, clss, s, d
         self.edge_indxs = edge_indxs
         self.has_instance = True
+
+        self.vars = {
+            'adj': self.dms,
+            'service_time': self.s,
+            'clss': self.clss,
+            'demand': self.demands
+        }
+
+
+class InsertCheapestHCARP(BaseHCARP):
     
-    def calc_cost(self, path):
-        return self.s[path].sum() + self.dms[path[:-1], path[1:]].sum()
-    def calc_demand(self, path):
-        return self.demands[path].sum()
+    def calc_obj(self, path):
+        return -(self.s[path].sum() + self.dms[path[:-1], path[1:]].sum())
+
+    def is_valid(self, path):
+        if self.demands[path].sum() > 1:
+            return False
+        return True
 
     def __call__(self, variant='P', is_local_search=True):
         assert self.has_instance, "please import instance first"
@@ -33,74 +45,35 @@ class InsertCheapestHCARP:
             edges = np.where(self.clss==p)[0]
             for e in edges:
                 paths = [routes[m] + [e] for m in self.M]
-                idxs = [i for i, path in enumerate(paths) if self.calc_demand(path) <= 1]
-                costs = [-self.calc_cost(paths[i]) for i in idxs]
-                idx = np.random.choice(idxs, size=1, p=softmax(costs))[0]
+                idxs = [i for i, path in enumerate(paths) if self.is_valid(path)]
+                costs = [self.calc_obj(paths[i]) for i in idxs]
+                idx = np.random.choice(idxs, size=1, p=convert_prob(costs))[0]
                 routes[idx] = paths[idx]
         
-        routes = np.int32([a for tour in routes for a in tour])
+        routes = np.int32([a for tour in routes for a in tour])[None]
 
-        
-        vars = {
-            'adj': self.dms[None],
-            'service_time': self.s[None],
-            'clss': self.clss[None],
-            'demand': self.demands[None]
-        }
         if is_local_search:
-            tours = local_search(vars, variant=variant, actions=routes[None], is_train=False)
-            return get_Ts(vars, tours_batch=tours)
-        return get_Ts(vars, actions=routes[None])
+            tours_batch = ls(self.vars, variant=variant, actions=routes)
+            return get_Ts(self.vars, tours_batch=tours_batch)
+        return get_Ts(self.vars, actions=routes)
 
-class EAHCARP:
-    def __init__(self, n_population=500, n_epoch=500, pathnament_size=4, mutation_rate=0.5, crossover_rate=0.9):
-        self.has_instance = False
+class EAHCARP(BaseHCARP):
+    def __init__(self, n_population=500, pathnament_size=4, mutation_rate=0.5, crossover_rate=0.9):
+        super().__init__()
         self.n_population = n_population
-        self.n_epoch = n_epoch
         self.pathnament_size = pathnament_size
         self.mutation_rate = mutation_rate
         self.crossover_rate = crossover_rate
         self.idxs_org = np.arange(n_population)
-        self.variant = None
-
-    def import_instance(self, f):
-        dms, P, M, demands, clss, s, d, edge_indxs = import_instance(f)
-        self.dms, self.P, self.M = dms, P, M
-        self.demands, self.clss, self.s, self.d = demands, clss, s, d
-        self.edge_indxs = edge_indxs
-        self.has_instance = True
-        self.vars = {
-            'adj': self.dms[None],
-            'service_time': self.s[None],
-            'clss': self.clss[None],
-            'demand': self.demands[None]
-        }
-
-        self.vars_batch={
-            'adj': [self.dms]*self.n_population,
-            'service_time': [self.s]*self.n_population,
-            'clss': [self.clss]*self.n_population,
-            'demand': [self.demands]*self.n_population
-        }
 
     def is_valid(self, routes):
         if (self.demands[gen_tours(routes)].sum(-1) > 1).sum() > 0:
             return False
         return True
 
-    def convert_prob(self, arr):
-        """
-            arr must be negative
-        """
-        min_value = np.min(arr)
-        shifted_arr = arr - min_value + 1e-10
-        prob = shifted_arr / np.sum(shifted_arr)
-        return prob
-
     def calc_obj(self, population):
-        n = len(population)
         w = np.array([1e3, 1e1, 1e-1])
-        rewards = get_Ts(self.vars_batch, tours_batch=gen_tours_batch(population))
+        rewards = get_Ts(self.vars, actions=population)
         obj = rewards @ w
         return -obj
     
@@ -126,22 +99,21 @@ class EAHCARP:
             child = self._cross_over(p1, p2, k)
         return child
 
-    def mutate(self, tours):
+    def mutate(self, tours, variant):
         n = len(tours)
-        tours = local_search(self.vars, variant=self.variant, actions=tours[None], is_train=False)
+        tours = ls(self.vars, variant=variant, actions=tours[None])
         return deserialize_tours(tours[0], n)
     
-    def __call__(self, variant='P', n_el=20):
+    def __call__(self, n_epoch=500, variant='P'):
         assert self.has_instance, "please import instance first"
-        self.variant = variant
         self.l_early = []
         population = self.init_population()
-        for epoch in range(self.n_epoch):
+        for epoch in range(n_epoch):
             # selecting two of the best options we have (elitism)
             obj = self.calc_obj(population)
             idxs = np.argsort(obj)
             new_population = [population[idxs[-1]], population[idxs[-2]]]
-            prob =self.convert_prob(obj)
+            prob = convert_prob(obj)
 
             for i in range(len(population) // 2 - 1):
                 # CROSSOVER
@@ -157,67 +129,43 @@ class EAHCARP:
 
                 # MUTATION
                 if random() <self.mutation_rate:
-                    child1 = self.mutate(child1)
-                    child2 = self.mutate(child1)
+                    child1 = self.mutate(child1, variant)
+                    child2 = self.mutate(child1, variant)
 
                 new_population.extend([child1, child2])
 
             population = new_population
-            if epoch % 10 == 0:
-                self.l_early.append(self.calc_obj(population).max())
-                print(f"epoch {epoch}:", self.l_early[-1])
+            # if epoch % 10 == 0:
+            #     print(f"epoch {epoch}:", self.l_early[-1])
 
-            if np.mean(self.l_early[-n_el:]) == self.l_early[-1] and len(self.l_early) > n_el:
-                break
 
         routes = population[self.calc_obj(population).argmax()]
         return get_Ts(self.vars, actions=routes[None])
     
 
-class ACOHCARP:
-    def __init__(self, n_ant=100, n_epoch=500, alpha=1.0, beta=1.0, rho=0.5, del_tau=1.0, k=4):
-        self.has_instance = False
+class ACOHCARP(BaseHCARP):
+    def __init__(self, n_ant=100, alpha=1.0, beta=1.0, rho=0.5, del_tau=1.0, k=4):
+        super().__init__()
         self.n_ant = n_ant
-        self.n_epoch = n_epoch
         self.alpha = alpha
         self.beta = beta
         self.rho = rho
         self.del_tau = del_tau
         self.k = k
 
-    def import_instance(self, f):
-        dms, P, M, demands, clss, s, d, edge_indxs = import_instance(f)
-        self.dms, self.P, self.M = dms, P, M
-        self.demands, self.clss, self.s, self.d = demands, clss, s, d
-        self.edge_indxs = edge_indxs
-        self.has_instance = True
-        self.vars = {
-            'adj': self.dms[None],
-            'service_time': self.s[None],
-            'clss': self.clss[None],
-            'demand': self.demands[None]
-        }
-
-        self.vars_batch={
-            'adj': [self.dms]*self.n_ant,
-            'service_time': [self.s]*self.n_ant,
-            'clss': [self.clss]*self.n_ant,
-            'demand': [self.demands]*self.n_ant
-        }
     def is_valid(self,route):
         if self.demands[route].sum() > 1:
             return False
         return True
     
     def calc_obj(self, population):
-        n = len(population)
         w = np.array([1e3, 1e1, 1e-1])
-        rewards = get_Ts(self.vars_batch, tours_batch=gen_tours_batch(population))
+        rewards = get_Ts(self.vars, actions=population)
         obj = rewards @ w
         return -obj
 
     def init_population(self):
-        pheromones = np.zeros_like(self.dms) # (17, 17)
+        pheromones = np.zeros_like(self.dms) 
         heads = []
         for _ in range(self.n_ant):
             head = permutation(range(1, len(self.s)))[:self.k]
@@ -225,18 +173,19 @@ class ACOHCARP:
                 head = permutation(range(1, len(self.s)))[:self.k]
             heads.append(head)
 
-        heads = np.array(heads) # (50, 4)
+        heads = np.array(heads) 
         np.add.at(pheromones, (heads[:,:-1], heads[:,1:]), self.del_tau)
         np.add.at(pheromones, (np.zeros_like(heads[:, 0]), heads[:, 0]), self.del_tau)
         return heads, pheromones
     
-    def __call__(self, variant='P', is_local_search = True):
-
+    def __call__(self, n_epoch=500, variant='P', is_local_search = True):
+        assert self.has_instance, "please import instance first"
+        
         heads, pheromones = self.init_population()
         i_max, max_obj = 0, -np.inf
         
         elitist_epochs = []
-        for epoch in range(self.n_epoch):
+        for epoch in range(n_epoch):
             elitist_ants = []
             for head in heads:
                 tour = head.tolist()
@@ -256,20 +205,21 @@ class ACOHCARP:
 
             if is_local_search:
                 n = len(elitist_ants[0])
-                tours = local_search(self.vars_batch, variant=variant, actions=elitist_ants, is_train=False)
+                tours = ls(self.vars, variant=variant, actions=elitist_ants)
                 elitist_ants = deserialize_tours_batch(tours, n)
 
+            # Update elite pheromones
             obj = self.calc_obj(elitist_ants)
             i_objmax = obj.argmax()
             if obj[i_objmax] > max_obj:
                 max_obj = obj[i_objmax]
-                i_max = i_objmax
-            elitist = np.int32(elitist_ants[obj.argmax()])
+                i_max = epoch
+            elitist = np.int32(elitist_ants[i_objmax])
             pheromones[elitist[:-1], elitist[1:]] += self.del_tau
             pheromones *= (1 - self.rho)
 
             elitist_epochs.append(elitist)
-            if epoch % 10 == 0:
-                print(f"epoch {epoch}:", max_obj)
+            # if epoch % 10 == 0:
+            #     print(f"epoch {epoch}:", max_obj)
         
         return get_Ts(self.vars, actions=elitist_epochs[i_max][None])
