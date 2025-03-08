82c2d8febba19ced1b789cfbc11b2d594ac31230
quangvan99 <vanquang.tpa@gmail.com>
2025-03-08 11:45:28 +0700

    fix pathdata 2

--- train.py
+++ train.py
@@ -28,9 +28,9 @@ def parse_args():
     parser.add_argument('--num_workers', type=int, default=24, help='Number of workers for data loader') 
     parser.add_argument('--accelerator', type=str, default='gpu', help='Training accelerator')
     parser.add_argument('--devices', type=int, default=1, help='Number of devices to use')
-    parser.add_argument('--path_train_data', type=str, default="../data/train_data.cache", help='path_train_data')
-    parser.add_argument('--path_val_data', type=str, default="../data/val_data.cache", help='path_val_data')
-    parser.add_argument('--path_test_data', type=str, default="../data/test_data.cache", help='path_test_data')
+    parser.add_argument('--path_train_data', type=str, default="../data/train_data.data", help='path_train_data')
+    parser.add_argument('--path_val_data', type=str, default="../data/val_data.data", help='path_val_data')
+    parser.add_argument('--path_test_data', type=str, default="../data/test_data.data", help='path_test_data')
     
     return parser.parse_args()
 
